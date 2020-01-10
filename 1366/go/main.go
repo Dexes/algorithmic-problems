ed8e18943923e58c2173aae68a8a76e491008b20
Artem Maksunov <maksunov@skbkontur.ru>
2020-01-10 11:50:20 +0500

    1366: go

---
+++ 1366/go/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"math/big"
+	"os"
+)
+
+func ToInt(data []byte) int64 {
+	var n int64
+	for _, b := range data {
+		if b < '0' || b > '9' {
+			break
+		}
+
+		n = n*10 + int64(b-'0')
+	}
+
+	return n
+}
+
+func GetResult(amount int64) string {
+	// http://mathworld.wolfram.com/Subfactorial.html
+
+	current := big.NewInt(0)
+	increase := big.NewInt(1)
+	decrease := big.NewInt(-1)
+	even := false
+
+	for i := int64(0); i <= amount; i++ {
+		current.Mul(current, big.NewInt(i))
+		if even = !even; even {
+			current.Add(current, increase)
+		} else {
+			current.Add(current, decrease)
+		}
+	}
+
+	return current.String()
+}
+
+func GetAllInput() []byte {
+	in := bufio.NewReader(os.Stdin)
+	bytes, _ := in.ReadBytes(0)
+
+	return bytes
+}
+
+func main() {
+	println(GetResult(ToInt(GetAllInput())))
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ## Volume 1 (1000–1099)
 - [1038. Spell Checker](https://acm.timus.ru/problem.aspx?num=1038): [go](1038/go/main.go)
+
+## Volume 4 (1300–1399)
+- [1366. Presents](https://acm.timus.ru/problem.aspx?num=1366): [go](1366/go/main.go)
