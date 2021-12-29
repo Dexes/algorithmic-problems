e1a793f2641e14babeb854b0e0e27f671f8d428f
Максунов Артём Дмитриевич <maksunov@skbkontur.ru>
2021-12-29 09:57:47 +0500

    LeetCode

---
+++ leetcode/0116. Populating Next Right Pointers in Each Node/go/main.go
@@ -0,0 +1,53 @@
+package main
+
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+	Next  *Node
+}
+
+type Queue []*Node
+
+func (q Queue) Push(nodes ...*Node) Queue {
+	for _, node := range nodes {
+		if node != nil {
+			q = append(q, node)
+		}
+	}
+
+	return q
+}
+
+func (q Queue) Pop() (*Node, Queue) {
+	return q[0], q[1:]
+}
+
+func (q Queue) IsEmpty() bool {
+	return len(q) == 0
+}
+
+func makeQueue(root *Node, maxDepth int) Queue {
+	result := make(Queue, 0, 1<<(maxDepth-1))
+	return result.Push(root)
+}
+
+func connect(root *Node) *Node {
+	var (
+		level, levelLength = 1, 1
+		node, right        *Node
+		queue              = makeQueue(root, 12)
+	)
+
+	for !queue.IsEmpty() {
+		node, queue = queue.Pop()
+		queue = queue.Push(node.Right, node.Left)
+		node.Next, right = right, node
+
+		if levelLength--; levelLength == 0 {
+			right, level, levelLength = nil, level+1, 1<<level
+		}
+	}
+
+	return root
+}
