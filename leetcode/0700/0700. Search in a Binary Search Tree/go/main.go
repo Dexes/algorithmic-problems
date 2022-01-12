72a8e4c40b50c0664e4311b007cc22f3beb09556
Максунов Артём Дмитриевич <maksunov@skbkontur.ru>
2022-01-12 07:13:58 +0500

    LeetCode

---
+++ leetcode/0700/0700. Search in a Binary Search Tree/go/main.go
@@ -0,0 +1,19 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil || root.Val == val {
+		return root
+	}
+
+	if val < root.Val {
+		return searchBST(root.Left, val)
+	}
+
+	return searchBST(root.Right, val)
+}
