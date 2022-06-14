b9508b745359088ad594a851804923793853cf01
Artem Maksunov <admaksunov@avito.ru>
2022-06-14 22:21:30 +0500

    leetcode

--- leetcode/0000/0014. Longest Common Prefix/go/main.go
+++ leetcode/0000/0014. Longest Common Prefix/go/main.go
@@ -1,12 +1,9 @@
 package main
 
 func longestCommonPrefix(data []string) string {
-	dataLength := len(data)
-	firstWordLength := len(data[0])
-
-	for charIndex := 0; charIndex < firstWordLength; charIndex++ {
+	for charIndex := 0; charIndex < len(data[0]); charIndex++ {
 		char := data[0][charIndex]
-		for i := 1; i < dataLength; i++ {
+		for i := 1; i < len(data); i++ {
 			if len(data[i]) <= charIndex || data[i][charIndex] != char {
 				return data[0][:charIndex]
 			}
