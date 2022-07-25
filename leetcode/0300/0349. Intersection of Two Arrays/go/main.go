781eeb903eab4aa92b00c1ba1ace4931dbb0eb34
Artem Maksunov <admaksunov@avito.ru>
2022-07-26 01:25:41 +0500

    leetcode

--- leetcode/0300/0349. Intersection of Two Arrays/go/main.go
+++ leetcode/0300/0349. Intersection of Two Arrays/go/main.go
@@ -1,33 +1,23 @@
 package main
 
 func intersection(nums1 []int, nums2 []int) []int {
-	return intersect(toSet(nums1), toSet(nums2))
-}
+	if len(nums1) > len(nums2) {
+		nums1, nums2 = nums2, nums1
+	}
 
-func intersect(a map[int]bool, b map[int]bool) []int {
-	result := make([]int, 0, min(len(a), len(b)))
-	for k, _ := range a {
-		if b[k] {
-			result = append(result, k)
+	set := make(map[int]struct{}, len(nums1))
+	for i := 0; i < len(nums1); i++ {
+		set[nums1[i]] = struct{}{}
+	}
+
+	wIndex := 0
+	for i := 0; i < len(nums2); i++ {
+		if _, ok := set[nums2[i]]; ok {
+			nums1[wIndex] = nums2[i]
+			wIndex++
+			delete(set, nums2[i])
 		}
 	}
 
-	return result
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-
-	return b
-}
-
-func toSet(a []int) map[int]bool {
-	result := make(map[int]bool)
-	for i := 0; i < len(a); i++ {
-		result[a[i]] = true
-	}
-
-	return result
+	return nums1[:wIndex]
 }
