40df91e34e93e1c08922c21829458e9247d8a2c5
Artem Maksunov <admaksunov@avito.ru>
2022-11-14 01:26:04 +0300

    leetcode

--- leetcode/0100/0151. Reverse Words in a String/go/main.go
+++ leetcode/0100/0151. Reverse Words in a String/go/main.go
@@ -3,19 +3,19 @@ package main
 import "strings"
 
 func reverseWords(s string) string {
-	sb, leftIndex := strings.Builder{}, 0
-	for rightIndex := len(s) - 1; rightIndex >= 0; rightIndex = leftIndex {
-		for ; rightIndex >= 0 && s[rightIndex] == ' '; rightIndex-- {
+	builder := strings.Builder{}
+	for left, right := 0, len(s)-1; right >= 0; right = left {
+		for ; right >= 0 && s[right] == ' '; right-- {
 		}
 
-		for leftIndex = rightIndex; leftIndex >= 0 && s[leftIndex] != ' '; leftIndex-- {
+		for left = right; left >= 0 && s[left] != ' '; left-- {
 		}
 
-		if leftIndex != rightIndex {
-			sb.WriteString(s[leftIndex+1 : rightIndex+1])
-			sb.WriteByte(' ')
+		if left != right {
+			builder.WriteString(s[left+1 : right+1])
+			builder.WriteByte(' ')
 		}
 	}
 
-	return sb.String()[:sb.Len()-1]
+	return builder.String()[:builder.Len()-1]
 }
