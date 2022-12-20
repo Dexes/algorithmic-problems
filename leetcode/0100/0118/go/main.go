9173ae54deb55ec328ecb5b24e30b6ea0979772f
Dexes <dex111000@gmail.com>
2022-12-20 20:51:44 +0300

    leetcode

--- leetcode/0100/0118/go/main.go
+++ leetcode/0100/0118/go/main.go
@@ -4,7 +4,7 @@ func generate(numRows int) [][]int {
 	result := make([][]int, 0, numRows)
 	result = append(result, []int{1}, []int{1, 1})
 
-	for i := 2; i <= numRows; i++ {
+	for i := 2; i < numRows; i++ {
 		current, prev := make([]int, i+1), result[i-1]
 		current[0], current[i] = 1, 1
 		for j := 1; j < i; j++ {
