3138b0736a4b77a8ee2003da87f72186f3add7ad
Dexes <dex111000@gmail.com>
2023-01-21 18:10:34 +0300

    leetcode

--- leetcode/1900/1929/go/main.go
+++ leetcode/1900/1929/go/main.go
@@ -1,10 +1,9 @@
 package main
 
 func getConcatenation(nums []int) []int {
-	length := len(nums)
-	result := make([]int, 2*length)
-	for i := 0; i < length; i++ {
-		result[i], result[length+i] = nums[i], nums[i]
+	result := make([]int, 2*len(nums))
+	for i := 0; i < len(nums); i++ {
+		result[i], result[len(nums)+i] = nums[i], nums[i]
 	}
 
 	return result
