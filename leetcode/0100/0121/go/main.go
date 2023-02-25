a097ee11bd57ba2337ab4a70caa18b7d820337cf
Dexes <dex111000@gmail.com>
2023-02-25 03:04:28 +0300

    leetcode

--- leetcode/0100/0121/go/main.go
+++ leetcode/0100/0121/go/main.go
@@ -1,15 +1,15 @@
 package main
 
 func maxProfit(prices []int) int {
-	result, min := 0, 10000
-	for i := 0; i < len(prices); i++ {
+	result, min := 0, prices[0]
+	for i := 1; i < len(prices); i++ {
 		if prices[i] < min {
 			min = prices[i]
 			continue
 		}
 
-		if prices[i]-min > result {
-			result = prices[i] - min
+		if x := prices[i] - min; x > result {
+			result = x
 		}
 	}
 
