5f4aa1f0cfa7dc7575357634c570e1fe95acaad2
Artem Maksunov <admaksunov@avito.ru>
2023-04-17 13:56:32 +0500

    leetcode

--- leetcode/0000/0007/go/main.go
+++ leetcode/0000/0007/go/main.go
@@ -1,17 +1,22 @@
 package main
 
+const (
+	min = -1 << 31
+	max = 1<<31 - 1
+)
+
 func reverse(x int) int {
 	result, sign := 0, 1
 	if x < 0 {
 		x, sign = -x, -sign
 	}
 
-	for x > 0 {
-		result, x = result*10+x%10, x/10
+	for ; x > 0; x /= 10 {
+		result = (result * 10) + (x % 10)
 	}
 
 	result *= sign
-	if result < -1<<31 || result > 1<<31-1 {
+	if result < min || result > max {
 		return 0
 	}
 
