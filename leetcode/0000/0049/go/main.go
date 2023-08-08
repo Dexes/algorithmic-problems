c54e55ee6deeda7b859fe14eb52c36d9b7526833
Artem Maksunov <admaksunov@avito.ru>
2023-08-08 11:27:52 +0300

    leetcode

--- leetcode/0000/0049/go/main.go
+++ leetcode/0000/0049/go/main.go
@@ -4,37 +4,23 @@ type mask [26]byte
 
 func groupAnagrams(strings []string) [][]string {
 	anagrams := make(map[mask][]string)
-	for i := 0; i < len(strings); i++ {
-		insert(anagrams, strings[i])
+	for _, s := range strings {
+		insert(anagrams, s)
 	}
 
-	return toArray(anagrams)
-}
-
-func insert(anagrams map[mask][]string, s string) {
-	m := toMask(s)
-	list := anagrams[m]
-
-	if list == nil {
-		list = make([]string, 0, 32)
-	}
-
-	anagrams[m] = append(list, s)
-}
-
-func toMask(s string) (result mask) {
-	for i := 0; i < len(s); i++ {
-		result[s[i]-'a']++
-	}
-
-	return result
-}
-
-func toArray(anagrams map[mask][]string) [][]string {
 	result := make([][]string, 0, len(anagrams))
 	for _, x := range anagrams {
 		result = append(result, x)
 	}
 
 	return result
+}
+
+func insert(anagrams map[mask][]string, s string) {
+	var m mask
+	for i := 0; i < len(s); i++ {
+		m[s[i]-'a']++
+	}
+
+	anagrams[m] = append(anagrams[m], s)
 }
