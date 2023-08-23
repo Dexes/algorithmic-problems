de17f7859f37662401341ef701eaf7d78dcc3019
Dexes <dex111000@gmail.com>
2023-08-24 01:36:53 +0300

    leetcode

---
+++ leetcode/2800/2828/go/main.go
@@ -0,0 +1,15 @@
+package main
+
+func isAcronym(words []string, s string) bool {
+	if len(words) != len(s) {
+		return false
+	}
+
+	for i, w := range words {
+		if w[0] != s[i] {
+			return false
+		}
+	}
+
+	return true
+}
